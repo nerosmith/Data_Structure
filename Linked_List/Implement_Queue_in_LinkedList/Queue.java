cff7258cf1b1aafca3845dabf3f29801de1a2062
KaiJie Zhang <ak47u0110@gmail.com>
2019-08-21 16:42:14 +0800

    ggggggg

--- Linked_List/Implement_Queue_in_LinkedList/Queue.java
+++
@@ -1,9 +0,0 @@
-package com.nerosmith.java.Linked_List.Implement_Queue_in_LinkedList;
-
-public interface Queue<E> {
-    int getSize();
-    boolean isEmpty();
-    void enqueue(E e);
-    E dequeue();
-    E getFront();
-}
--- Linked_List/Implement_Stack_in_LinkedList/Stack.java
+++
@@ -1,9 +0,0 @@
-package com.nerosmith.java.Linked_List.Implement_Stack_in_LinkedList;
-
-public interface Stack<E> {
-    int getSize();
-    boolean isEmpty();
-    void push(E e);
-    E pop();
-    E peek();
-}
